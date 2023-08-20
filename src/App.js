fe3cedbf7174375b1dabb3dae917481a2fc7b0f2
udaysk3 <burluudaysantoshkumar3@gmail.com>
2023-08-20 22:19:27 +0530

    Prod changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setsearchTerm] = useState('');
   const searchMovies = async (title) => {
-    const response = await fetch(`http://www.omdbapi.com/?s=${title}&apikey=263d22d8`);
+    const response = await fetch(`http://www.omdbapi.com/?s=${title}&apikey=ddfd1f63`);
     const data = await response.json();
     setMovies(data.Search);
   }
